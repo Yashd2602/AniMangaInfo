20fe6d4d33ce1bcb9d55d64c9d66d8ceeda8a9d7
Yashd2602 <yashdubey2602@gmail.com>
2026-01-20 17:38:35 +0530

    Deploy site

--- src/Redux/Store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import aniMangaReducer from "./aniMangaSlice"
-
-export const store = configureStore({
-    reducer:{
-        aniManga: aniMangaReducer,
-    }
-})
